7c0792cb15294c0e9e251db0dcede1f462d83101
nolanjannotta <anjannotta@verizon.net>
2021-12-23 20:37:07 -0800

    first commit

---
+++ script.py
@@ -0,0 +1,31 @@
+import enchant
+import random
+
+
+
+letters = "uqtianc"
+easyLetters = "eariotnslc"
+coolLetters = "nsipeq"
+constant = "u"
+
+correctWords = []
+
+isEnglish = enchant.Dict("en_US")
+
+
+print("finding words...")
+def findWord():    
+    wordSize = random.randint(4,10) 
+    word = "".join(random.choices(letters, k=wordSize))
+    # print(word, wordSize)
+    if constant in word:
+        if isEnglish.check(word):
+            if word not in correctWords:
+                correctWords.append(word)
+
+    
+
+for i in range(0, 1000000):
+
+    findWord()
+print(correctWords)
